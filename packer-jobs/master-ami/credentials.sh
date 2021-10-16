7fbaca61a8d27c6d829861fe3cc1de0a78d99130
Admin <admin@olawunmi.local>
2021-10-16 19:46:59 -0400

    push to repo

--- packer-jobs/master-ami/credentials.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Set Environment Variables
-echo "setting environment variables....."
-export AWS_ACCESS_KEY="AKIA4CSTSZZHZFLHIGNF"
-export AWS_SECRET_KEY="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"
-export AWS_REGION="us-east-1"
