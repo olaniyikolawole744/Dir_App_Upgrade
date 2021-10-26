6382d27cdd2735543214b8d4d6cce73d4c86da0f
olaniyi <olaniyikolawole744@gmail.com>
2021-10-26 08:25:03 -0400

    push to repo

--- scripts/dev_playbook.sh
+++ scripts/dev_playbook.sh
@@ -3,4 +3,4 @@
 #PLAY ANSIBLE BOOK
 cd ansible-jobs
 ls
-sh 'ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playdev.yml -i inventory/hosts/ec2.py --vault-password-file $ANSIBLE_VAULT_PASSWORD_FILE'
+sh 'ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playdev.yml -i inventory/hosts/ec2.py --vault-password-file $ANSIBLE_VAULT_PASSWORD_FILE -vvvvv'
